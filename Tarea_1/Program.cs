7c7245087211bb610d58b999a32cdbfcd5f45414
MargelCastro <margelgabriel@gmail.com>
2024-09-12 19:51:50 -0600

    requerimientos del programa

--- Tarea_1/Program.cs
+++ Tarea_1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*1. Margel Castro: Gestión de Productos Descripción: Implementar un sistema que permita 
+  -- registrar    --modificar   
+  --eliminar   y  --listar productos de una tienda. 
+Cada producto debe tener código, nombre, precio y cantidad en stock. */
 Console.WriteLine("Hello, World!");
