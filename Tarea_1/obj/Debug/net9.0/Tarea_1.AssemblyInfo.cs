c37e8cbb727f55849d93016c2326e574f5615060
MargelCastro <margelgabriel@gmail.com>
2024-09-18 20:25:24 -0600

    creacion de clase y metodos

--- Tarea_1/obj/Debug/net9.0/Tarea_1.AssemblyInfo.cs
+++ Tarea_1/obj/Debug/net9.0/Tarea_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f23e967cda96a53dd0372082e0bbb5a09de36f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d998b2b09ba45d041223d8affe28c094d604d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
